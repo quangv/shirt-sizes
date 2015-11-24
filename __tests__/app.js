05e849d191e407deca177e909f70fa69eff2d990
Quang Van <quangvvv@gmail.com>
2015-11-24 00:08:52 -0500

    app contains results

--- __tests__/app.js
+++ __tests__/app.js
@@ -6,6 +6,7 @@ import TestUtils from 'react-addons-test-utils';
 
 const App = require('../containers/App');
 const ChoiceBox = require('../components/ChoiceBox');
+const Results = require('../components/Results');
 
 describe('App', function(){
   it('should exists', function(){
@@ -20,6 +21,15 @@ describe('App', function(){
     let output = renderer.getRenderOutput();
     
     expect(output.props.children[1].type).toBe(ChoiceBox);
+
+  });
+
+  it('should render Results', function(){
+    let renderer = TestUtils.createRenderer();
+    renderer.render(<App />);
+    let output = renderer.getRenderOutput();
+    
+    expect(output.props.children[2].type).toBe(Results);
 
   });
 })
--- containers/App.jsx
+++ containers/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChoiceBox from '../components/ChoiceBox'
+import Results from '../components/Results'
 
 class App extends React.Component {
   render(){
@@ -7,6 +8,7 @@ class App extends React.Component {
       <div>
         <h1>Select a Shirt Size</h1>
         <ChoiceBox />
+        <Results />
       </div>
     )
   }
