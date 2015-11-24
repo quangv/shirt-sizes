b478a9e1907e3166505bdb38f6bf5940c27e3d20
Quang Van <quangvvv@gmail.com>
2015-11-24 00:19:43 -0500

    add choices to choicebox

--- __tests__/choiceBox.js
+++ __tests__/choiceBox.js
@@ -4,11 +4,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-import ChoiceBox from '../components/ChoiceBox';
+const ChoiceBox = require('../components/ChoiceBox');
 
 describe('ChoiceBox', function(){
+
+  let output;
+  beforeEach(function(){
+    let renderer = TestUtils.createRenderer();
+    renderer.render(<ChoiceBox />);
+    output = renderer.getRenderOutput();
+  });
+
   it('should exists', function(){
     let choiceBox = TestUtils.renderIntoDocument(<ChoiceBox />);
     expect(TestUtils.isCompositeComponent(choiceBox)).toBeTruthy();
+  });
+
+  it('should render 4 choices', function(){
+    expect(output.props.children.length).toBe(4)
+  });
+
+  describe('choices', function(){
+    it('first choice should be small', function(){
+      expect(output.props.children[0].props.title).toBe('small');
+    });
+    it('second choice should be medium', function(){
+      expect(output.props.children[1].props.title).toBe('medium');
+    });
+    it('first choice should be large', function(){
+      expect(output.props.children[2].props.title).toBe('large');
+    });
+    it('first choice should be xl', function(){
+      expect(output.props.children[3].props.title).toBe('xl');
+    });
   });
 })
--- components/ChoiceBox.jsx
+++ components/ChoiceBox.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Choice from './Choice'
 
 class ChoiceBox extends React.Component {
   render(){
-    return  <div>ChoiceBox</div>
+    return (
+      <div>
+        <Choice title="small" />
+        <Choice title="medium" />
+        <Choice title="large" />
+        <Choice title="xl" />
+      </div>
+    )
   }
 }
 
